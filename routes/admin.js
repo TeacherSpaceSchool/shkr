702a5b35fd69ddc891b0fe9447d86470fcc5c3b3
Space School <space.school.kg@gmail.com>
2019-02-11 17:29:13 +0600

    Console route.

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-const dirname1 = __dirname.replace('\\routes', '')
-console.log(dirname1)
+let  dirname1 = __dirname.replace('\\routes', '')
+dirname1 = __dirname.replace('/routes', '')
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.sendFile(path.join(dirname1, 'admin', 'index.html'));
